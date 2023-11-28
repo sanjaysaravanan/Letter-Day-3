8a8fdae1adc9ee3f2b8373d8f9eccfca70207b46
Sanjay Saravanan <sanjay.saravanan@encora.com>
2023-11-29 00:38:21 +0530

    added changes

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ async function displayTextOneLetterAtATime(text, outputElement) {
     if (index < text.length) {
       outputElement.textContent += text.charAt(index);
       index++;
-      setTimeout(() => displayNextLetter(resolve), 1); // Adjust the delay (in milliseconds) between letters
+      setTimeout(() => displayNextLetter(resolve), 100); // Adjust the delay (in milliseconds) between letters
     } else {
       resolve();
     }
